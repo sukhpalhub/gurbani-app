017f0e02dffe9663250496fce3b0a15026c4d194
Sukhpal Singh <sukhpalmail@protonmail.com>
2022-04-14 18:03:36 +1000

    Added bootstrap and search panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Card, Col, Container, Form, InputGroup, Navbar, Row} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {DashSquare, PlusSquare, Search} from "react-bootstrap-icons";
 
 function App() {
   const [data, setData] = useState({
     'name_gurmukhi': null,
   });
+
+  const [showSearch, setShowSearch] = useState(true);
 
   useEffect(() => {
     window.api.getScripture().then((data) => {
@@ -14,9 +19,48 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      { data.name_gurmukhi }
-    </div>
+      <Container fluid style={{height: '100%'}}>
+        <Row className="gurmukhi-row">
+          <Col></Col>
+        </Row>
+        <Row className="punjabi-row">
+          <Col></Col>
+        </Row>
+        <Row className="english-row">
+          <Col></Col>
+        </Row>
+          {
+              showSearch &&
+              <Card id="searchBox">
+                  <Navbar bg="light" className="d-block">
+                      <DashSquare
+                          className="float-end me-2"
+                          onClick={() => {
+                              setShowSearch(! showSearch);
+                          }}
+                      />
+                  </Navbar>
+                  <Card.Body bg="primary">
+                      <InputGroup>
+                          <Form.Control />
+                          <InputGroup.Text>
+                            <Search />
+                          </InputGroup.Text>
+                      </InputGroup>
+                  </Card.Body>
+              </Card>
+          }
+          {
+              ! showSearch &&
+              <PlusSquare
+                  id="searchBoxMaxButton"
+                  className="me-2 mb-2"
+                  onClick={() => {
+                      setShowSearch(! showSearch);
+                  }}
+              />
+          }
+      </Container>
   );
 }
 
